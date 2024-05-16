875a88a7730730e0b9ec105f7ccf25269bcf1885
Saad Akbar <102979976+saadakbar1@users.noreply.github.com>
2024-05-16 15:00:02 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,3 +54,24 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+// JavaScript to toggle between light and dark mode
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggler = document.getElementById('theme-toggler');
+    let isDarkMode = false;
+  
+    themeToggler.addEventListener('click', () => {
+      isDarkMode =!isDarkMode;
+      document.body.classList.toggle('dark-mode', isDarkMode);
+  
+      // Switch icons
+      if (isDarkMode) {
+        themeToggler.classList.remove('bxs-sun');
+        themeToggler.classList.add('bx-moon');
+      } else {
+        themeToggler.classList.remove('bx-moon');
+        themeToggler.classList.add('bxs-sun');
+      }
+    });
+  });
+  
